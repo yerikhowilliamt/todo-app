cee3f30798292d104858ae1ccb4869a5aa406746
Yerikho William Tasilima <yerikhowilliamt@gmail.com>
2024-07-13 02:53:27 +0700

    fix: done-list

--- script.js
+++ script.js
@@ -1,100 +1,118 @@
-// Create variable
-let tasks = JSON.parse(localStorage.getItem("tasks")) || []
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-const todoForm = document.getElementById("todoForm")
-const inputTask = document.getElementById("inputTask")
-const inputLevel = document.getElementById("selectLevel")
-const addButton = document.getElementsByClassName("add-btn")
-const todoList = document.querySelector(".todo-list")
-const doneList = document.querySelector(".done-list")
-const checkbox = todoList.querySelector("input")
-const li = document.getElementsByTagName("li")
-const ulDoneList = doneList.getElementsByTagName("ul")
+const todoForm = document.getElementById("todoForm");
+const inputTask = document.getElementById("inputTask");
+const inputLevel = document.getElementById("selectLevel");
+const addButton = document.getElementById("addButton");
+const todoList = document.querySelector(".todo-list");
+const doneList = document.querySelector(".done-list");
+const deleteAllButton = document.querySelector(".delete-all");
 
-const now = new Date()
-const day = now.getDay()
-const date = now.getDate()
-const month = now.getMonth()
-const year = now.getFullYear()
-const addNumber = (number) => {
-  if (number < 10) {
-    return `0${number}`
-  } else {
-    return number
-  }
+function toggleDeleteAllButton() {
+  deleteAllButton.style.display =
+    doneList.childElementCount > 0 ? "block" : "none";
 }
 
+function loadTasks() {
+  tasks.forEach((task) => {
+    if (task.isCompleted) {
+      createTaskDone(task);
+    } else {
+      createTaskTodo(task);
+    }
+  });
 
-
-const dayName = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"]
-const time = `${dayName[day]} ${addNumber(date)}-${addNumber(month + 1)}-${year}`
-
-if (localStorage.getItem("tasks")) {
-  tasks.map((task) => createTaskTodo(task))
+  toggleDeleteAllButton();
 }
 
-if (localStorage.getItem("tasks")) {
-  tasks.map((task) => task.isCompleted == true ? createTaskDone(task) : "")
-}
+loadTasks();
 
 todoForm.addEventListener("submit", (e) => {
-  e.preventDefault()
+  e.preventDefault();
 
-  const inputValue = inputTask.value
-  const levelValue = inputLevel.value
-  const dateValue = time
-
-  let number = Math.random() * 500
-  let id = Math.round(number)
-
-  if (inputValue == "") {
-    return
-  }
-
+  const inputValue = inputTask.value;
+  const levelValue = inputLevel.value;
+  const date = new Date();
   const task = {
-    id: id,
+    id: Date.now(),
     name: inputValue,
     level: levelValue,
-    date: dateValue,
-    isCompleted: false
-  }
+    date: date.toLocaleString(),
+    isCompleted: false,
+  };
 
-  tasks.push(task)
-  localStorage.setItem("tasks", JSON.stringify(tasks))
+  tasks.push(task);
+  saveTasks();
+  createTaskTodo(task);
 
-  createTaskTodo(task)
-
-  todoForm.reset()
-  inputTask.focus()
-})
+  todoForm.reset();
+  inputTask.focus();
+});
 
 todoList.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete-task")) {
-    const taskId = e.target.closest("li").id
-
-    
-    deleteTask(taskId)
+    const taskId = e.target.closest("li").id;
+    deleteTask(taskId);
+  } else if (e.target.type === "checkbox") {
+    const taskId = e.target.id;
+    updateTask(taskId, e.target.checked);
   }
-})
+});
 
-todoList.addEventListener("input", (e) => {
-  const taskId = e.target.closest("li").id
-  updateTask(taskId, e.target)
-})
+doneList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-task")) {
+    const taskId = e.target.closest("li").id;
+    deleteTask(taskId);
+  } else if (e.target.classList.contains("delete-all-tasks")) {
+    deleteAllData();
+  }
+});
 
+deleteAllButton.addEventListener("click", () => {
+  deleteAllData();
+});
 
 function createTaskTodo(task) {
-  const taskElement = document.createElement("li")
-
-  taskElement.setAttribute("id", task.id)
-
-  if (task.isCompleted) {
-    taskElement.classList.add("complete")
-  }
+  const taskElement = document.createElement("li");
+  taskElement.setAttribute("id", task.id);
 
   const taskElementTodo = `
     <div class="todos">
-      <input type="checkbox" name="tasks" id="${task.id}" ${task.isCompleted ? "checked" : ""}>
+      <input type="checkbox" name="tasks" id="${task.id}" ${
+    task.isCompleted ? "checked" : ""
+  }>
+      <div class="task-container">
+        <div class="task">
+          <h6>TO-DO :</h6>
+          <span>${task.name}</span>
+        </div>
+        <div class="level">
+          <h6>Level Prioritas :</h6>
+          <span>${task.level}</span>
+        </div>
+        <div class="date">
+          <h6>Waktu :</h6>
+          <span>${task.date}</span>
+        </div>
+      </div>
+      <button type="button" title="Hapus ${
+        task.name
+      } Task?" class="delete-task">
+        Hapus
+      </button>
+    </div>
+  `;
+
+  taskElement.innerHTML = taskElementTodo;
+  todoList.appendChild(taskElement);
+}
+
+function createTaskDone(task) {
+  const taskElement = document.createElement("li");
+  taskElement.setAttribute("id", task.id);
+
+  const taskElementDone = `
+    <div class="dones">
       <div class="task-container">
         <div class="task">
           <h6>TO-DO :</h6>
@@ -113,18 +131,49 @@ function createTaskTodo(task) {
         Hapus
       </button>
     </div>
-  `
+  `;
 
-  taskElement.innerHTML = taskElementTodo
-
-  todoList.appendChild(taskElement)
+  taskElement.innerHTML = taskElementDone;
+  doneList.appendChild(taskElement);
 }
 
-function createTaskDone(task) {
+function deleteTask(taskId) {
+  tasks = tasks.filter((task) => task.id !== parseInt(taskId));
+  saveTasks();
+  document.getElementById(taskId).remove();
 
-  const taskElement = document.createElement("li")
+  toggleDeleteAllButton();
+}
 
-  taskElement.setAttribute("id", task.id)
+function updateTask(taskId, isChecked) {
+  const taskIndex = tasks.findIndex((task) => task.id === parseInt(taskId));
+
+  if (taskIndex !== -1) {
+    tasks[taskIndex].isCompleted = isChecked;
+
+    if (isChecked) {
+      const task = tasks[taskIndex];
+      moveTaskToDone(task);
+
+      const taskElement = document.getElementById(taskId);
+      todoList.removeChild(taskElement);
+    } else {
+      const task = tasks[taskIndex];
+      moveTaskToTodoList(task);
+
+      const taskElement = document.getElementById(taskId);
+      doneList.removeChild(taskElement);
+    }
+
+    saveTasks();
+  }
+
+  toggleDeleteAllButton();
+}
+
+function moveTaskToDone(task) {
+  const taskElement = document.createElement("li");
+  taskElement.setAttribute("id", task.id);
 
   const taskElementDone = `
     <div class="dones">
@@ -142,35 +191,25 @@ function createTaskDone(task) {
           <span>${task.date}</span>
         </div>
       </div>
+      <button type="button" title="Hapus ${task.name} Task?" class="delete-task">
+        Hapus
+      </button>
     </div>
-  `
+  `;
 
-  taskElement.innerHTML = taskElementDone
-
-  doneList.appendChild(taskElement)
+  taskElement.innerHTML = taskElementDone;
+  doneList.appendChild(taskElement);
 }
 
-function deleteTask(taskId) {
-  tasks = tasks.filter((task) => task.id !== parseInt(taskId))
+function deleteAllData() {
+  tasks = tasks.filter((task) => !task.isCompleted);
 
-  localStorage.setItem("tasks", JSON.stringify(tasks))
+  doneList.innerHTML = "";
 
-  document.getElementById(taskId).remove()
+  toggleDeleteAllButton();
+  saveTasks();
 }
 
-function updateTask(taskId, el) {
-  const task = tasks.find((task) => task.id === parseInt(taskId))
-  const parent = el.closest("li")
-
-  task.isCompleted = !task.isCompleted
-
-    if (task.isCompleted) {
-      parent.classList.add("complete")
-      createTaskDone(task)
-    } else {
-      parent.classList.remove("complete")
-    }
-
-  localStorage.setItem("tasks", JSON.stringify(tasks))
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-
